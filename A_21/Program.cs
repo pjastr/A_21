30f86dcb059fbd86c1e894d967c6cf13454b4ee0
Piotrek <@>
2017-11-18 14:16:18 +0100

    przeslanie plikow

---
+++ A_21/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wiek;
+            wiek = 23;
+            Console.WriteLine("Wiek: {0}", wiek);
+            Console.ReadKey();
+        }
+    }
+}
